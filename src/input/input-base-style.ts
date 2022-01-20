23c18ffce1586253517c112fbdc50b7e50695518
UpperCod <mtrujillo@outlook.cl>
2022-01-20 14:07:13 -0300

    separate styles for reuse between components

---
+++ src/input/input-base-style.ts
@@ -0,0 +1,82 @@
+import { css } from "atomico";
+import {
+    tokensSpace,
+    tokensFont,
+    tokensSize,
+    tokensBorder,
+    tokensColor,
+    tokensShadow,
+    tokensOpacity,
+    tokensTransition,
+} from "../tokens";
+
+export const inputBaseStyle = [
+    tokensSpace,
+    tokensFont,
+    tokensSize,
+    tokensBorder,
+    tokensColor,
+    tokensShadow,
+    tokensOpacity,
+    tokensTransition,
+    css`
+        :host {
+            --color-fill: var(--color-current-layer, var(--color-input-fill));
+            --color-divide: var(--color-input-divide);
+            --color-contrast: var(
+                --color-current-contrast,
+                var(--color-input-contrast)
+            );
+            --color-status: var(--color-input-status);
+            --line-opacity: var(--opacity-disabled);
+            --shadow: none;
+            ---space-x: var(--space-x);
+            ---padding: 0 var(---space-x);
+            ---height: var(--size-min);
+            ---font-size: var(--font-size);
+            font-size: var(---font-size);
+        }
+        :host([shadow]) {
+            --shadow: var(--shadow-action);
+        }
+        .input {
+            display: grid;
+            min-width: 100%;
+            background: var(--color-fill);
+            color: var(--color-contrast);
+            position: relative;
+            border-radius: var(--border-radius);
+            border: var(--border-width) solid var(--color-divide);
+            box-sizing: border-box;
+            box-shadow: var(--shadow);
+        }
+        .input-content {
+            display: grid;
+            min-height: var(---height);
+            align-items: center;
+            padding: var(---padding);
+            grid-gap: var(--space-between);
+            grid-template-columns: var(--columns);
+            position: relative;
+        }
+        .input-line {
+            width: 100%;
+            height: var(--border-width);
+            padding: 0 var(---space-x);
+            box-sizing: border-box;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            z-index: 3;
+            transform: translateY(100%);
+            opacity: var(--line-opacity);
+            transition: var(--transition-x0);
+        }
+        .input-line-fill {
+            width: 100%;
+            height: 100%;
+            border-radius: 1rem;
+            background: var(--color-status);
+        }
+    `,
+];
--- src/input/input.tsx
+++ src/input/input.tsx
@@ -2,19 +2,10 @@ import { Props, c, useProp, useRef, css, useHost } from "atomico";
 import { useSlot } from "@atomico/hooks/use-slot";
 import { useRender } from "@atomico/hooks/use-render";
 import { useDisabled } from "@atomico/hooks/use-disabled";
-import {
-    tokensSpace,
-    tokensSize,
-    tokensBorder,
-    tokensColor,
-    tokensShadow,
-    tokensOpacity,
-    tokensTransition,
-    tokensFont,
-} from "../tokens";
 import { InputGenericProps } from "../props";
 import customElements from "../custom-elements";
 import { serialize } from "../utils";
+import { inputBaseStyle } from "./input-base-style";
 
 function input({ type, status, ...props }: Props<typeof input>) {
     const [, setValue] = useProp("value");
@@ -58,23 +49,25 @@ function input({ type, status, ...props }: Props<typeof input>) {
             }}
         >
             <div class="input">
-                <slot
-                    ref={refSlotPrefix}
-                    name="prefix"
-                    class={serialize(!slotPrefix.length && "hidden")}
-                ></slot>
-                <slot
-                    ref={refSlotLabel}
-                    class={serialize(!slotLabel.length && "hidden")}
-                ></slot>
-                <slot name="input"></slot>
-                <slot
-                    ref={refSlotSuffix}
-                    name="suffix"
-                    class={serialize(!slotSuffix.length && "hidden")}
-                ></slot>
-                <div class="input-line">
-                    <div class="input-line-fill"></div>
+                <div class="input-content">
+                    <slot
+                        ref={refSlotPrefix}
+                        name="prefix"
+                        class={serialize(!slotPrefix.length && "hidden")}
+                    ></slot>
+                    <slot
+                        ref={refSlotLabel}
+                        class={serialize(!slotLabel.length && "hidden")}
+                    ></slot>
+                    <slot name="input"></slot>
+                    <slot
+                        ref={refSlotSuffix}
+                        name="suffix"
+                        class={serialize(!slotSuffix.length && "hidden")}
+                    ></slot>
+                    <div class="input-line">
+                        <div class="input-line-fill"></div>
+                    </div>
                 </div>
             </div>
             <style>{`
@@ -121,49 +114,8 @@ input.props = {
 };
 
 input.styles = [
-    tokensSpace,
-    tokensFont,
-    tokensSize,
-    tokensBorder,
-    tokensColor,
-    tokensShadow,
-    tokensOpacity,
-    tokensTransition,
+    inputBaseStyle,
     css`
-        :host {
-            --color-fill: var(--color-current-layer, var(--color-input-fill));
-            --color-divide: var(--color-input-divide);
-            --color-contrast: var(
-                --color-current-contrast,
-                var(--color-input-contrast)
-            );
-            --line-opacity: var(--opacity-disabled);
-            --color-status: var(--color-input-status);
-            --shadow: var(--shadow-action);
-            ---space-x: var(--space-x);
-            ---padding: 0 var(---space-x);
-            ---height: var(--size-min);
-            ---font-size: var(--font-size);
-            font-size: var(---font-size);
-        }
-        :host([shadow]) {
-            box-shadow: var(--shadow);
-        }
-        .input {
-            display: grid;
-            min-width: 100%;
-            min-height: var(---height);
-            align-items: center;
-            padding: var(---padding);
-            position: relative;
-            background: var(--color-fill);
-            color: var(--color-contrast);
-            border-radius: var(--border-radius);
-            border: var(--border-width) solid var(--color-divide);
-            box-sizing: border-box;
-            grid-gap: var(--space-between);
-            grid-template-columns: var(--columns);
-        }
         ::slotted([slot="input"]) {
             width: 100%;
             height: 100%;
@@ -178,25 +130,6 @@ input.styles = [
             outline: none;
             padding: 0px;
             letter-spacing: unset;
-        }
-        .input-line {
-            width: 100%;
-            height: var(--border-width);
-            padding: 0 var(---space-x);
-            box-sizing: border-box;
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            z-index: 3;
-            transform: translateY(100%);
-            opacity: var(--line-opacity);
-            transition: var(--transition-x0);
-        }
-        .input-line-fill {
-            width: 100%;
-            height: 100%;
-            border-radius: 1rem;
-            background: var(--color-status);
         }
         .hidden {
             display: none;
