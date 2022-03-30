ad3a3debde2b4f34b35be5d65faf0e3919395ece
UpperCod <mtrujillo@outlook.cl>
2022-03-30 12:24:00 -0300

    fix system.defined

--- src/button/button-active.tsx
+++ src/button/button-active.tsx
@@ -1,6 +1,6 @@
 import { Host, c, css, useState } from "atomico";
 import { getCoordinates } from "@atomico/hooks/use-click-coordinates";
-import system from "../system";
+import customElement from "../system";
 
 function buttonActive(): Host<{ setEvent(event: MouseEvent): void }> {
     const [state, setState] = useState<{
@@ -74,4 +74,4 @@ buttonActive.styles = css`
 
 export const ButtonActive = c(buttonActive);
 
-system.define("button-active", ButtonActive);
+customElement.define("button-active", ButtonActive);
