1a80abd12785d8ce6f492ee07810beb3a3b1ea84
UpperCod <mtrujillo@outlook.cl>
2022-04-11 02:06:29 -0400

    examples

--- src/_doc/example-contact-app.doc.tsx
+++ src/_doc/example-contact-app.doc.tsx
@@ -12,6 +12,8 @@ ${(<App></App>)}
 
 ## Sources
 
-1. TSX [stackblitz.com](https://stackblitz.com/edit/formilk-example-contact-app?file=src%2Fapp.tsx)
-2. JSX [webcomponents.dev](https://webcomponents.dev/edit/collection/FnaPy15Ple9r6IJKNYDx/kmPhuXMrvAl1vLM8ytB7/src/index.jsx?p=stories)
+1. Atomico TSX [stackblitz.com](https://stackblitz.com/edit/formilk-example-contact-app?file=src%2Fapp.tsx)
+2. Atomico JSX [webcomponents.dev](https://webcomponents.dev/edit/collection/FnaPy15Ple9r6IJKNYDx/kmPhuXMrvAl1vLM8ytB7/src/index.jsx?p=stories)
+2. React Tsx [stackblitz.com](https://stackblitz.com/edit/formilk-example-contact-app-react?file=src%2FApp.tsx&terminal=dev)
+2. Preact Tsx [stackblitz.com](https://stackblitz.com/edit/formilk-example-contact-app-preact?file=src%2Fapp.tsx&terminal=dev)
 `;
